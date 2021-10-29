1ebfbfe79573476b88b3563a7b2826b7e1d75014
ngandon <nicolas.gandon78@gmail.com>
2021-10-30 01:39:06 +0200

    core app

--- trigger_lambda/src/App.js
+++ trigger_lambda/src/App.js
@@ -1,22 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [awsid, setAwsId] = useState("empty");
+  const [awssecret, setAwsSecret] = useState("empty");
+
+  const handleAwsIdInput = event => {
+    setAwsId({ awsid: event.target.value });  
+  };
+
+  const handleAwsSecretInput = event => {
+    setAwsSecret({ awssecret: event.target.value });  
+  };
+
+  const logValue = () => {
+    console.log(awsid, awssecret);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <input onChange={handleAwsIdInput} placeholder="Enter AWS access key id"/>
+          <input onChange={handleAwsSecretInput} placeholder="Enter AWS secret access key"/>       
+          
+        </div>
+        <div>
+          <button onClick={logValue}>Check server status</button>
+        </div>
+
       </header>
     </div>
   );
